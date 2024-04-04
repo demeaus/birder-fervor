607ac7ee485b6793bf28ba4a190dce5fd3724510
Demeaus Wong <demeaus.wong@gmail.com>
2024-04-04 10:43:29 -0600

    Fixed marker not rendering x3.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,16 @@ import { useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useObservations } from "../hooks/useObservations";
 import { useNavigate, useParams } from "react-router-dom";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 const initalPosition = { lat: 39.75, lng: -104.95 };
 
