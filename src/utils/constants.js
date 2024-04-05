b8a80293b92e6a5749f69b8df0edb1ef62c6bfd3
Demeaus Wong <demeaus.wong@gmail.com>
2024-04-04 22:04:59 -0600

    Selecting a pin on the map centers it in the map.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const GEOAPIFY_API_URL = 'https://api.geoapify.com/v1/geocode/';
 export const EBIRD_CHECKLIST_URL = 'https://ebird.org/checklist/';
 
 // React Select styles
-export const controlStyles = "rounded-full border-2 border-yellow-500 text-sm px-4 py-2 bg-yellow-100 my-1 min-w-64";
+export const controlStyles = "rounded-full border-2 border-yellow-500 text-sm px-4 py-2 bg-yellow-50 my-1 min-w-64";
 export const menuStyles = "bg-gray-100 text-sm px-2 py-1 z-50 ";
 export const optionStyles = "border-b py-1";
 export const placeholderStyles = "text-gray-400";
