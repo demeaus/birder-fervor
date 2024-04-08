911dede51008e9f36f8b697b10346805c4284d34
Demeaus Wong <demeaus.wong@gmail.com>
2024-04-08 12:08:26 -0600

    Moved function to convert coordinates to address to React Query.

--- src/components/SelectRegion.jsx
+++ src/components/SelectRegion.jsx
@@ -10,16 +10,22 @@ import {
   placeholderStyles,
   indicatorStyles,
 } from "../utils/constants";
+import { useAddress } from "../hooks/useAddress";
 
 // User can select a state/province to get relevant species for that region
 function SelectRegion() {
+  const navigate = useNavigate();
+  const { layer } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  const { isLoading: isLoadingAddress, error, address = {} } = useAddress();
+
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [isLoadingAutocomplete, setIsLoadingAutocomplete] = useState(false);
   const [selectedRegion, setSelectedRegion] = useState(null);
-  const navigate = useNavigate();
-  const { layer } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
 
   // Handle clearing or selection of input from dropdown
   function handleChange(e, { action }) {
@@ -36,17 +42,20 @@ function SelectRegion() {
 
     // Region was selected
     const selected = e.value;
-    // console.log(selected);
+    console.log(selected);
     setSelectedRegion({
       value: {
         layer: selected.layer,
         lat: selected.latitude,
         lng: selected.longitude,
       },
-      label: selected.formattedAddress,
+      label: `${selected.countryFlag} ${selected.formattedAddress}`,
     });
     navigate(`/${selected.layer}`);
-    setSearchParams({ lat: selected.latitude, lng: selected.longitude });
+    setSearchParams(
+      { lat: selected.latitude, lng: selected.longitude },
+      { replace: true },
+    );
   }
 
   // Handle typing in input of dropdown
@@ -54,22 +63,29 @@ function SelectRegion() {
     if (input.length > 1 && query !== input) setQuery(input);
   }
 
-  // // Syncs URL with region selector
-  // // URL -> dropdown
-  // useEffect(() => {
-  //   if (!suggestions.length && layer && !selectedRegion) {
-  //     const result = getRegionName(layer);
-  //     if (result?.name && result?.parent) {
-  //       console.log("converted: ", result.name, result.parent);
-  //       setSelectedRegion({
-  //         value: layer,
-  //         label: `${result.name}, ${result.parent}`,
-  //       });
-  //     } else {
-  //       navigate("/");
-  //     }
-  //   }
-  // }, [layer, suggestions.length, navigate]);
+  // Syncs URL with region selector
+  // URL -> dropdown
+  useEffect(() => {
+    if (!suggestions.length && address?.formattedAddress && !selectedRegion) {
+      setSelectedRegion({
+        value: {
+          layer: layer,
+          lat: lat,
+          lng: lng,
+        },
+        label: `${address.countryFlag} ${address.formattedAddress}`,
+      });
+    }
+  }, [
+    address?.countryFlag,
+    address?.formattedAddress,
+    lat,
+    lng,
+    layer,
+    selectedRegion,
+    suggestions.length,
+    navigate,
+  ]);
 
   // Fetches list of autocomplete suggestions for search input
   useEffect(() => {
@@ -80,6 +96,7 @@ function SelectRegion() {
 
         // Format suggestions for React Select component
         if (suggestionsList) {
+          console.log(suggestionsList);
           setSuggestions(
             suggestionsList.map((obj) => ({
               value: obj,
@@ -123,7 +140,7 @@ function SelectRegion() {
         unstyled={true}
         backspaceRemovesValue={true}
         isClearable={true}
-        isLoading={isLoadingAutocomplete}
+        isLoading={isLoadingAutocomplete || isLoadingAddress}
         value={selectedRegion}
         placeholder="Enter state or province..."
       />
---
+++ src/hooks/useAddress.js
@@ -0,0 +1,25 @@
+/**
+ * useSpeciesCodes.js
+ */
+import { useQuery } from "@tanstack/react-query";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getAddressbyCoordinates } from "../services/apiRadar";
+
+export function useAddress() {
+    const { layer } = useParams();
+    const [searchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+
+    const {
+        isLoading,
+        data: address,
+        error,
+    } = useQuery({
+        queryKey: ["address", { layer, lat, lng }],
+        queryFn: () => getAddressbyCoordinates(layer, lat, lng),
+        enabled: !!(layer && lat && lng)
+    });
+
+    return { isLoading, error, address };
+}
