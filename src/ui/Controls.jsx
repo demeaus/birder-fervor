b63b1ed2027c6437ce6efdc3ae0d3f6a6d0e7493
Demeaus Wong <demeaus.wong@gmail.com>
2024-04-10 21:42:00 -0600

    Added styles for larger screens.

--- src/ui/Controls.jsx
+++ src/ui/Controls.jsx
@@ -13,7 +13,7 @@ function Controls({ children }) {
       <div
         className={`flex flex-col items-start gap-1 rounded-b-lg border-2 border-t-0 border-solid border-gray-400 bg-gray-700 px-4 pt-3 text-sm text-yellow-50 ${isOpen ? "" : "hidden "}`}
       >
-        <h1 className="px-4 text-xs">
+        <h1 className="px-4 text-xs lg:text-lg ">
           Search for recent sightings by region and species:
         </h1>
         <div className="flex w-full max-w-sm flex-col items-start gap-1 px-4 pb-3 sm:left-0 sm:top-0 sm:min-w-fit sm:max-w-screen-lg sm:flex-row sm:items-center sm:justify-center sm:gap-4">
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="relative z-20 w-full border-2 border-solid border-gray-400 bg-gray-700 p-2">
+    <header className="relative z-20 w-full border-2 border-solid border-gray-400 bg-gray-700 p-2 lg:px-6 lg:py-4 ">
       <Link
-        className="text-2xl uppercase tracking-widest text-yellow-400"
+        className="text-2xl uppercase tracking-widest text-yellow-400 lg:text-4xl"
         to="/"
       >
         Birder Fervor
